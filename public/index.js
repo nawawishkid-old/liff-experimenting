036a700c1dd98786b61b9f7201697c0bed35d178
Nawawish Samerpark <hydrokidz@gmail.com>
2018-12-19 02:23:22 +0700

    updated: message type

--- public/index.js
+++ public/index.js
@@ -13,35 +13,68 @@ function liffInit() {
     )
   );
 
+  const ACTION_URI = "https://nawawish.me";
   const message = {
     text: {
       type: "text",
       text: "You've successfully sent a message! Hooray!"
     },
-    templateButton: {
-      type: "template",
-      altText: "This is a buttons template",
-      template: {
-        type: "buttons",
-        thumbnailImageUrl:
-          "https://images.unsplash.com/photo-1543363950-c78545037afc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
-        title: "Menu",
-        text: "Please select",
-        defaultAction: {
-          type: "uri",
-          label: "View detail",
-          uri: "https://nawawish.me"
-        },
-        actions: [
-          {
-            type: "uri",
-            label: "View detail",
-            uri: "https://nawawish.me"
-          }
-        ]
-      }
-    },
+    templateButton: getTemplate("button", {
+      thumbnailImageUrl:
+        "https://images.unsplash.com/photo-1543363950-c78545037afc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
+      title: "Menu",
+      text: "Please select",
+      defaultAction: {
+        type: "uri",
+        label: "View detail",
+        uri: ACTION_URI
+      },
+      actions: getUriActions(3)
+    }),
+    templateConfirm: getTemplate("confirm", {
+      text: "Are you sure?",
+      actions: getUriActions(2, [{ label: "Yes" }, { label: "No" }])
+    }),
+    templateCarousel: getTemplate("carousel", {
+      columns: getCarouselColumns(
+        "https://images.unsplash.com/photo-1545070255-fdc9a55d32f2",
+        "https://images.unsplash.com/photo-1545132147-d037e6c54cfd",
+        "https://images.unsplash.com/photo-1545121064-3e8e1241e291"
+      )
+    }),
     image: {},
     location: {}
+  };
+
+  const getTemplate = (type, options) => ({
+    type: "template",
+    altText: `This is a ${type} template.`,
+    template: { type, ...options }
+  });
+
+  const getCarouselColumns = (...imageSrcs) =>
+    imageSrcs.map((src, index) => ({
+      thumbnailImageUrl: src,
+      title: `Item ${index + 1}`,
+      text: `This is item no. ${index + 1}`,
+      actions: getUriActions(3)
+    }));
+
+  const getUriActions = (number, ...options) => {
+    const actions = [];
+    let option;
+
+    for (let i = 0; i < number; i++) {
+      option = typeof options[i].type !== "undefined" ? options[i] : {};
+
+      actions.push({
+        type: "uri",
+        label: `option #${i + 1}`,
+        uri: ACTION_URI,
+        ...option
+      });
+
+      return actions;
+    }
   };
 }
