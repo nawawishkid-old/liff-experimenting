6205fff0a553023ba5f3fa4a84059714c8bfcae8
Nawawish Samerpark <hydrokidz@gmail.com>
2018-12-19 03:08:55 +0700

    updated: refactored

--- public/index.js
+++ public/index.js
@@ -1,9 +1,71 @@
 window.onload = () =>
-  liff.init(data => liffInit(data), err => console.log("err: ", err));
+  liff.init(data => init(data), err => console.log("err: ", err));
 
-function liffInit() {
-  console.log("liffInit()");
+function init() {
+  console.log("init()");
 
+  const message = getMessage();
+
+  [...document.getElementsByClassName("send-message-button")].forEach(node =>
+    node.addEventListener(
+      "click",
+      () =>
+        console.log("send message type: " + node.dataset.messageType) ||
+        liff.sendMessages([message[node.dataset.messageType]])
+    )
+  );
+}
+
+function getMessage() {
+  const {
+    getTemplate,
+    getCarouselColumns,
+    getImageCarouselColumns,
+    getUriActions
+  } = getHelpers();
+  const TEMPLATE_BUTTONS = "buttons";
+  const TEMPLATE_CAROUSEL = "carousel";
+  const TEMPLATE_CONFIRM = "confirm";
+  const TEMPLATE_IMAGE_CAROUSEL = "image_carousel";
+  const images = [
+    "https://images.unsplash.com/photo-1545070255-fdc9a55d32f2",
+    "https://images.unsplash.com/photo-1545132147-d037e6c54cfd",
+    "https://images.unsplash.com/photo-1545121064-3e8e1241e291",
+    "https://images.unsplash.com/photo-1543363950-c78545037afc"
+  ];
+  const message = {
+    text: {
+      type: "text",
+      text: "You've successfully sent a message! Hooray!"
+    },
+    templateButton: getTemplate(TEMPLATE_BUTTONS, {
+      thumbnailImageUrl: images[3],
+      title: "Menu",
+      text: "Please select",
+      defaultAction: getUriActions(1, { label: "View detail" })[0],
+      actions: getUriActions(3)
+    }),
+    templateConfirm: getTemplate(TEMPLATE_CONFIRM, {
+      text: "Are you sure?",
+      actions: getUriActions(2, { label: "Yes" }, { label: "No" })
+    }),
+    templateCarousel: getTemplate(TEMPLATE_CAROUSEL, {
+      columns: getCarouselColumns(...images)
+    }),
+    templateImageCarousel: getTemplate(TEMPLATE_IMAGE_CAROUSEL, {
+      columns: getImageCarouselColumns(...images.map(img => ({ url: img })))
+    }),
+    image: {},
+    location: {}
+  };
+
+  console.log("message: ", message);
+
+  return message;
+}
+
+function getHelpers() {
+  const ACTION_URI = "https://nawawish.me";
   const getTemplate = (type, options) => ({
     type: "template",
     altText: `This is a ${type} template.`,
@@ -13,11 +75,7 @@ function liffInit() {
   const getImageCarouselColumns = (...objs) =>
     objs.map((obj, index) => ({
       imageUrl: obj.url,
-      action: {
-        type: "uri",
-        label: obj.label || `label ${index + 1}`,
-        uri: ACTION_URI
-      }
+      action: getUriActions(1, { label: obj.label })[0]
     }));
 
   const getCarouselColumns = (...imageSrcs) =>
@@ -47,55 +105,10 @@ function liffInit() {
     return actions;
   };
 
-  const TEMPLATE_BUTTONS = "buttons";
-  const TEMPLATE_CAROUSEL = "carousel";
-  const TEMPLATE_CONFIRM = "confirm";
-  const TEMPLATE_IMAGE_CAROUSEL = "image_carousel";
-  const ACTION_URI = "https://nawawish.me";
-  const images = [
-    "https://images.unsplash.com/photo-1545070255-fdc9a55d32f2",
-    "https://images.unsplash.com/photo-1545132147-d037e6c54cfd",
-    "https://images.unsplash.com/photo-1545121064-3e8e1241e291",
-    "https://images.unsplash.com/photo-1543363950-c78545037afc"
-  ];
-  const message = {
-    text: {
-      type: "text",
-      text: "You've successfully sent a message! Hooray!"
-    },
-    templateButton: getTemplate(TEMPLATE_BUTTONS, {
-      thumbnailImageUrl: images[3],
-      title: "Menu",
-      text: "Please select",
-      defaultAction: {
-        type: "uri",
-        label: "View detail",
-        uri: ACTION_URI
-      },
-      actions: getUriActions(3)
-    }),
-    templateConfirm: getTemplate(TEMPLATE_CONFIRM, {
-      text: "Are you sure?",
-      actions: getUriActions(2, { label: "Yes" }, { label: "No" })
-    }),
-    templateCarousel: getTemplate(TEMPLATE_CAROUSEL, {
-      columns: getCarouselColumns(...images)
-    }),
-    templateImageCarousel: getTemplate(TEMPLATE_IMAGE_CAROUSEL, {
-      columns: getImageCarouselColumns(...images.map(img => ({ url: img })))
-    }),
-    image: {},
-    location: {}
+  return {
+    getCarouselColumns,
+    getImageCarouselColumns,
+    getUriActions,
+    getTemplate
   };
-
-  console.log("message: ", message);
-
-  [...document.getElementsByClassName("send-message-button")].forEach(node =>
-    node.addEventListener(
-      "click",
-      () =>
-        console.log("send message type: " + node.dataset.messageType) ||
-        liff.sendMessages([message[node.dataset.messageType]])
-    )
-  );
 }
